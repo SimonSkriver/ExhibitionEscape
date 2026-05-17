59333f95b4aad8f3e7212ef924b11ed7e2e0a617
Simon Gade <simongade@duck.com>
2026-05-17 18:17:50 +0200

    Seagull Animations

--- Assets/Tech/Scripts/Seagull.cs
+++ Assets/Tech/Scripts/Seagull.cs
@@ -7,39 +7,38 @@ public class Seagull : MonoBehaviour
 
     [SerializeField] int range = 100;
     [SerializeField] int flySpeed = 10;
-    
     Vector3 flyDest;
 
-    void Awake() {
-        StartCoroutine(ChangeFlyDestination());
-    }
 
-    private void Update() {
+    void Awake() => StartCoroutine(ChangeFlyDestination());
+
+    void Update() {
         transform.position = Vector3.MoveTowards(transform.position, flyDest, flySpeed * Time.deltaTime);
         transform.LookAt(flyDest);
     }
 
-    // fly code
-    void Fly() {
-        float rndX = orbit.position.x + Random.Range(0, range);
-        float rndY = orbit.position.y + Random.Range(0, range * 0.2f);
-        float rndZ = orbit.position.z + Random.Range(0, range);
-        
-        flyDest = new Vector3(rndX, rndY, rndZ);
-    }
-
     IEnumerator ChangeFlyDestination() {
         while (true) {
-            Fly();
+            // Change destination point
+            float rndX = orbit.position.x + Random.Range(0, range);
+            float rndY = orbit.position.y + Random.Range(0, range * 0.2f);
+            float rndZ = orbit.position.z + Random.Range(0, range);
+            flyDest = new Vector3(rndX, rndY, rndZ);
+
+            // beak animation
+            Animator A = GetComponent<Animator>();
+            A.SetBool("openBeak", true);
+            yield return new WaitForSeconds(1);
+            A.SetBool("openBeak", false);
+            
             yield return new WaitUntil(() => Vector3.Distance(transform.position, flyDest) < 1);
         }
     }
 
-    // fly location = Random.Range(midIsland, midIsland + 100)
-
-
-    // RND = Random Vector3 (if destination is over 100 from midIsland, then multiply by 0.5f;
-    // newFlyDestination = transform.position + RND
-
-    // change fly location if destination is reached or Random Timer is hit
+    void OnDrawGizmos() {
+        Gizmos.color = Color.aquamarine;
+        Gizmos.DrawLine(transform.position, flyDest);
+        Gizmos.color = Color.mediumAquamarine;
+        Gizmos.DrawSphere(flyDest, 0.3f);
+    }
 }
