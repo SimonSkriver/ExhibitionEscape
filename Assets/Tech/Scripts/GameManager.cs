0fcc2c8f46c081d7993dccf5fb5dfc62d2fc3518
Simon Gade <simongade@duck.com>
2026-05-29 21:17:56 +0200

    Customization Camera Lerps

--- Assets/Tech/Scripts/GameManager.cs
+++ Assets/Tech/Scripts/GameManager.cs
@@ -3,9 +3,11 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public static Transform
-        CameraManager;
+        CameraManager,
+        customCam;
 
     private void Awake() {
         CameraManager = transform.Find("CameraManager");
+        customCam = CameraManager.GetChild(1);
     }
 }
