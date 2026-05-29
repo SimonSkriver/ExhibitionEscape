2b90e23325b8d165ab6b16410826ff5bd1adbeb7
Simon Gade <simongade@duck.com>
2026-05-29 14:34:16 +0200

    CameraManager

--- Assets/Tech/Scripts/UI_Scripts/PowerUpUI.cs
+++ Assets/Tech/Scripts/UI_Scripts/PowerUpUI.cs
@@ -5,7 +5,7 @@ public class PowerUpUI : MonoBehaviour
 {
     PowerUpUI Instance;
     VisualElement root;
-    Image imgJumpBoost;
+    Image imgJumpBoost, imgSpeedBoost, imgStrengthBoost;
 
     private void Awake() {
         if (Instance != null) { Destroy(gameObject); }
@@ -13,10 +13,18 @@ public class PowerUpUI : MonoBehaviour
 
         root = GetComponent<UIDocument>().rootVisualElement;
         imgJumpBoost = root.Q<Image>("JumpBoostIcon");
-        
+        imgSpeedBoost = root.Q<Image>("SpeedBoostIcon");
+        imgStrengthBoost = root.Q<Image>("StrengthBoostIcon");
+
         HideJumpBoostIcon();
+        HideSpeedBoostIcon();
+        HideStengthBoostIcon();
     }
 
     public void ShowJumpBoostIcon() => imgJumpBoost.style.display = DisplayStyle.Flex;
     public void HideJumpBoostIcon() => imgJumpBoost.style.display = DisplayStyle.None;
+    public void ShowSpeedBoostIcon() => imgSpeedBoost.style.display = DisplayStyle.Flex;
+    public void HideSpeedBoostIcon() => imgSpeedBoost.style.display = DisplayStyle.None;
+    public void ShowStengthBoostIcon() => imgStrengthBoost.style.display = DisplayStyle.Flex;
+    public void HideStengthBoostIcon() => imgStrengthBoost.style.display = DisplayStyle.None;
 }
