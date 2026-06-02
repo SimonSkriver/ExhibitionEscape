27c32cc0bbac7f74ad64554ff082d3a3e8963a81
SimonSkriver <simon.skriver@hotmail.com>
2026-06-02 13:55:17 +0200

    Made treasure chest and cutscene bool logic

---
+++ Assets/Tech/Scripts/Items/TreasureChest.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class TreasureChest : MonoBehaviour, IInteractable
+{
+    [SerializeField] Animator animator;
+    [SerializeField] Animator hatAnimator;
+    bool hasInteracted;
+
+    public void Interact()
+    {
+        animator.SetTrigger("OpenLid");
+        hatAnimator.SetTrigger("FlyUp");
+        hasInteracted = true;
+    }
+
+    public bool ShowOutline()
+    {
+        if (hasInteracted) return false;
+        else return true;
+    }
+}
