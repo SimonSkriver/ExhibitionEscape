fc658d4158c9f7b5651577779af790a8e437894c
SimonSkriver <simon.skriver@hotmail.com>
2026-06-05 15:02:24 +0200

    Update PlayerFallDamage.cs

--- Assets/Tech/Scripts/Player/PlayerFallDamage.cs
+++ Assets/Tech/Scripts/Player/PlayerFallDamage.cs
@@ -6,7 +6,6 @@ public class PlayerFallDamage : MonoBehaviour
     [Tooltip ("The minimum velocity before fall damage is applied")]
     [SerializeField] private float threshold = 3.5f;
     [SerializeField] private float dmgMultiplier = 2.5f;
-    [SerializeField] private float waterMoveSpeedMult = 0.7f;
     [SerializeField] private LayerMask water;
     [SerializeField] private Transform feet;
     
