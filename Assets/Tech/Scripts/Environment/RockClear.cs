b0086ed03c2bd3b893c8b2dc4f849147789dc33b
JakobStorm7913 <156937915+JakobStorm7913@users.noreply.github.com>
2026-06-08 15:10:24 +0200

    Boar update

--- Assets/Tech/Scripts/Environment/RockClear.cs
+++ Assets/Tech/Scripts/Environment/RockClear.cs
@@ -3,6 +3,15 @@ using UnityEngine;
 public class RockClear : MonoBehaviour, IInteractable
 {
     private Rigidbody[] rocks;
+    [SerializeField] private Animator animator;
+
+    public void Awake()
+    {
+         if (animator == null)
+        {
+            animator = GameObject.FindWithTag("Player").GetComponent<Animator>();
+        }
+    }
 
     public void Interact()
     {
@@ -12,6 +21,7 @@ public class RockClear : MonoBehaviour, IInteractable
         if (BoulderStrengthEffect.Instance.CanDestroyBoulders)
         {
             GetComponent<Collider>().enabled = false;
+            animator.SetTrigger("ATTACK");
             SFXManager.PlayEffect("BoulderDestroy");
             rocks = GetComponentsInChildren<Rigidbody>();
             foreach (Rigidbody rb in rocks)
